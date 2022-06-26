f80ad83f73957c1201b1d8db4aa8466baf4a4360
ecanvasser <eddiecanvassere9@gmail.com>
2022-06-25 18:25:58 -0700

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    
+    let result = [];
+    for (let i = 0; i <= word.length-1; i++) {
+        result.unshift(word[i]);
+    }
 
+    return result.join('')
 };
 
 // Do not edit below this line
