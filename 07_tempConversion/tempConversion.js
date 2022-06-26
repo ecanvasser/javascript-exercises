b60dba562c2912a884fa1dffc30bf15f9e1f5ec9
ecanvasser <eddiecanvassere9@gmail.com>
2022-06-26 15:44:20 -0700

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let cel = (far - 32) * (5/9)
+  return Number(cel.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let far = (cel * 9 / 5) + 32
+  return Number(far.toFixed(1));
 };
 
 // Do not edit below this line
