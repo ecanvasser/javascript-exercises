5e920a00bdd443f0f15aa67deb5761c23d56c191
ecanvasser <eddiecanvassere9@gmail.com>
2022-06-26 15:02:52 -0700

    Solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
-};
+    let nums = [num1, num2].sort();
 
+    if ((num1 < 0 || typeof num1 !== 'number') || (num2 < 0 || typeof num2 !== 'number')) {
+        return 'ERROR';
+    } else {
+        let sum = 0;
+        for (let i = nums[0]; i <= nums[1]; i++) {
+            sum += i;
+        };
+        return sum;
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
