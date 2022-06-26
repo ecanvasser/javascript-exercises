f83b0660c7f1c1fbc3e49233e8409fb3f96ead9a
ecanvasser <eddiecanvassere9@gmail.com>
2022-06-25 19:28:26 -0700

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
+    
+    let arr = Array.from(arguments)[0];
+    let args = Array.from(arguments).slice(1,);
 
+    for (const arg of args) {
+        if (arr.indexOf(arg) !== -1) {
+            arr.splice(arr.indexOf(arg), 1);
+        } else {
+            continue;
+        }
+    }
+
+    return arr;
+        
 };
 
 // Do not edit below this line
