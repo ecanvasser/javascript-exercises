2c9dfe5b702da322081207c8d16a5704aa9a5372
ecanvasser <eddiecanvassere9@gmail.com>
2022-06-25 18:27:53 -0700

    Exercise results in terminal error

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result;
 
+    let i = num;
+    while (i > 1) {
+        result += string;
+    };
+
+    return result
 };
 
 // Do not edit below this line
